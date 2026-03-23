12b1064c88eeeb999c1e5f3c21a8a7ab8d95c8a4
Aisling Acuna <aacuna1525@sdsu.edu>
2026-03-23 16:18:44 -0700

    updated documentation

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,8 @@
 G = 6.67430e-11        
 M_sun = 1.98847e30     
-R_sun = 6.957e8        
+R_sun = 6.957e8 # m       
 L_sun = 3.828e26       
 pc_to_m = 3.085677e16
 T_sun = 5777 # K
+yr_to_s = 3.154e7
+m_to_km = 0.001
